81aeb40d110b06555822607f7a1307e8b8ed3f64
marichu99 <martinmaati31@gmail.com>
2023-11-01 15:30:44 +0300

    Changes to the login page

--- src/main/java/com/servlet/auth/Login.java
+++ src/main/java/com/servlet/auth/Login.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet(urlPatterns = "/login",initParams = {
-    @WebInitParam(name = "username", value = "mabera"),
+    @WebInitParam(name = "email", value = "martin@gmail.com"),
     @WebInitParam(name = "password", value = "mabera")
 })
 public class Login extends HttpServlet{
@@ -21,16 +21,13 @@ public class Login extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-
-        String username = req.getParameter("username");
+        String username = req.getParameter("email");
         String password = req.getParameter("password");
         PrintWriter printWriter = resp.getWriter();
-
-
-        if(username.equals(getInitParameter("username")) && password.equals(getInitParameter("password"))){
+        if(username.equals(getInitParameter("email")) && password.equals(getInitParameter("password"))){
             printWriter.write("Welcome to Accounting");
-            // RequestDispatcher dispatcher = req.getRequestDispatcher("./home");
-            // dispatcher.forward(req, resp);
+            RequestDispatcher dispatcher = req.getRequestDispatcher("./home");
+            dispatcher.forward(req, resp);
         }else{
             printWriter.write("Invalid login details <a href=\".\">Login Again</a>");
         }
--- src/main/java/com/servlet/home/Home.java
+++ src/main/java/com/servlet/home/Home.java
@@ -5,10 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/home")
 public class Home extends HttpServlet{
 
 
@@ -16,13 +18,16 @@ public class Home extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("<b>Welcome to the Home Page </b>");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        resp.sendRedirect("./app/Home.html");
     }
+}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        // PrintWriter printWriter = resp.getWriter();
-        // printWriter.write("./");
-
-        resp.sendRedirect("./app/index.html");
-    }}
+   
